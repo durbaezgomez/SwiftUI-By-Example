1a8e5de5975833d9f84fb365a3557ad2be5c8b37
Dominik Urbaez Gomez <dominikurbaezgomez@gmail.com>
2020-10-19 12:40:39 +0200

    Two-way bindings in SwiftUI

--- SwiftUI By Example/Views/CheckoutView.swift
+++ SwiftUI By Example/Views/CheckoutView.swift
@@ -7,23 +7,53 @@
 
 import SwiftUI
 
+enum PaymentType: String {
+    case cash = "Cash"
+    case card = "Credit Card"
+    case points = "iDine Points"
+}
+
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
     
-    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
-    @State private var paymentType = 0
+    static let paymentTypes: [PaymentType] = [.cash, .card, .points]
+    @State private var paymentType: PaymentType = .cash
+    
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    
+    static let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 1
     
     var body: some View {
         VStack {
             Section {
-                VStack {
-                    Text("How do you want to pay?")
-                    Picker("", selection: $paymentType) {
+                Form {
+                    Picker("How do you want to pay?", selection: $paymentType) {
                         ForEach(Self.paymentTypes, id: \.self) {
-                            Text($0)
+                            Text($0.rawValue)
                         }
                     }
-                    .frame(width: 100, height: 100)
+                    Toggle(isOn: $addLoyaltyDetails.animation()) {
+                        Text("Add iDine loyalty card")
+                    }
+                    if addLoyaltyDetails {
+                        TextField("Enter your iDine ID", text: $loyaltyNumber)
+                    }
+                    Section(header: Text("Add a tip?")) {
+                        Picker("Percentage:", selection: $tipAmount) {
+                            ForEach(0 ..< Self.tipAmounts.count) {
+                                Text("\(Self.tipAmounts[$0])%")
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                    Section(header:
+                        Text("TOTAL: $100")
+                    ) {
+                        Button("Confirm order") {
+                            // place the order
+                        }
+                    }
                 }
             }
         }
